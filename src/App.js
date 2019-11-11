a78fe6cf0aec181209ed88d44fec7ab7678079ff
Anand Prabhu <anandprabhu.karthikeyan@coda.global>
2019-11-11 09:22:46 +0530

    Again checking

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. just checking. 
+          Edit <code>src/App.js</code> and save to reload. just checking. again checking. 
         </p>
         <a
           className="App-link"
